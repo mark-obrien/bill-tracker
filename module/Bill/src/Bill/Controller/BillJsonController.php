7a7b55a06799ca33051d04d36d9190c7dc6dd285
Mark <markob83@yahoo.com>
2014-10-26 14:47:17 -0500

    donut chart

--- module/Bill/src/Bill/Controller/BillJsonController.php
+++ module/Bill/src/Bill/Controller/BillJsonController.php
@@ -31,4 +31,16 @@ class BillJsonController extends AbstractBillController
         return $result;
 
     }
+
+    public function billAction(){
+
+        $repo = $this->getEntityManager()->getRepository('\Application\Entity\Bill');
+        $query = $repo->createQueryBuilder('c');
+        $data = $query->getQuery()->getResult(AbstractQuery::HYDRATE_ARRAY);
+
+        $result = new JsonModel($data);
+
+        return $result;
+
+    }
 } 
--- public/js/plugins/morris/morris-data.js
+++ public/js/plugins/morris/morris-data.js
@@ -1,87 +1,106 @@
 $(function() {
 
+    function drawAreaChart() {
+        $.getJSON("/bill/json/payment", function (json) {
+            var billAreaChart = [];
+            var billJson = json;
 
-    function drawChart() {
-        $.getJSON("/bill/json/payment", function (json) { // callback function which gets called when your request completes.
-            var myarray = [];
-            var myJSON = json;
-
-            for (var i = 0; i < myJSON.length; i++) {
-
-                var item = {
-                    "x": myJSON[i].date_json,
-                    "y":myJSON[i].running_balance
+            for (var i = 0; i < billJson.length; i++) {
+                var areaItem = {
+                    "x": billJson[i].date_json,
+                    "y":billJson[i].running_balance
                 };
 
-                myarray.push(item);
+                billAreaChart.push(areaItem);
             }
 
-            console.log(myarray);
-
-             Morris.Area({
+            Morris.Area({
                 element: 'morris-area-chart',
-                data: myarray,
+                data: billAreaChart,
                 xkey: 'x',
                 ykeys: ['y'],
                 pointSize: 2
             });
-
         });
     }
 
-    drawChart();
+    function drawDonutChart() {
+        $.getJSON("/bill/json/bill", function (json) {
+            var billDonut = [];
+            var billJson = json;
 
-    Morris.Donut({
-        element: 'morris-donut-chart',
-        data: [{
-            label: "Download Sales",
-            value: 12
-        }, {
-            label: "In-Store Sales",
-            value: 30
-        }, {
-            label: "Mail-Order Sales",
-            value: 20
-        }],
-        resize: true
-    });
+            for (var i = 0; i < billJson.length; i++) {
+                var item = {
+                    "label": billJson[i].creditor,
+                    "value":billJson[i].balance
+                };
 
-    Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            y: '2006',
-            a: 100,
-            b: 90
-        }, {
-            y: '2007',
-            a: 75,
-            b: 65
-        }, {
-            y: '2008',
-            a: 50,
-            b: 40
-        }, {
-            y: '2009',
-            a: 75,
-            b: 65
-        }, {
-            y: '2010',
-            a: 50,
-            b: 40
-        }, {
-            y: '2011',
-            a: 75,
-            b: 65
-        }, {
-            y: '2012',
-            a: 100,
-            b: 90
-        }],
-        xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
-        hideHover: 'auto',
-        resize: true
-    });
+                billDonut.push(item);
+                console.log(billDonut);
+            }
+
+            Morris.Donut({
+                element: 'morris-donut-chart',
+                data: billDonut,
+                resize: true
+            });
+        });
+    }
+
+    drawAreaChart();
+    drawDonutChart();
+
+    //Morris.Donut({
+    //    element: 'morris-donut-chart',
+    //    data: [{
+    //        label: "Download Sales",
+    //        value: 12
+    //    }, {
+    //        label: "In-Store Sales",
+    //        value: 30
+    //    }, {
+    //        label: "Mail-Order Sales",
+    //        value: 20
+    //    }],
+    //    resize: true
+    //});
+
+    //Morris.Bar({
+    //    element: 'morris-bar-chart',
+    //    data: [{
+    //        y: '2006',
+    //        a: 100,
+    //        b: 90
+    //    }, {
+    //        y: '2007',
+    //        a: 75,
+    //        b: 65
+    //    }, {
+    //        y: '2008',
+    //        a: 50,
+    //        b: 40
+    //    }, {
+    //        y: '2009',
+    //        a: 75,
+    //        b: 65
+    //    }, {
+    //        y: '2010',
+    //        a: 50,
+    //        b: 40
+    //    }, {
+    //        y: '2011',
+    //        a: 75,
+    //        b: 65
+    //    }, {
+    //        y: '2012',
+    //        a: 100,
+    //        b: 90
+    //    }],
+    //    xkey: 'y',
+    //    ykeys: ['a', 'b'],
+    //    labels: ['Series A', 'Series B'],
+    //    hideHover: 'auto',
+    //    resize: true
+    //});
 
 });
